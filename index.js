45bc4c35eeba3d05c2303860611ec3db29d00d89
tigercosmos <phy.tiger@gmail.com>
2019-12-19 04:00:19 +0800

    update

--- index.js
+++ index.js
@@ -24,7 +24,83 @@ async function GuessNumber(context) {
   await context.sendText('遊戲開始！輸入四個數字來猜！');
 }
 
+async function GuessNumber(context) {
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let i = Math.floor((Math.random() * arr.length));
+  const random1 = arr[i];
+  arr.splice(i, 1);
+  i = Math.floor((Math.random() * arr.length));
+  const random2 = arr[i];
+  arr.splice(i, 1);
+  i = Math.floor((Math.random() * arr.length));
+  const random3 = arr[i];
+  arr.splice(i, 1);
+  i = Math.floor((Math.random() * arr.length));
+  const random4 = arr[i];
+  arr.splice(i, 1);
+
+  let state = context.state;
+  state.number = {
+    start: true,
+    answer: `${random1}${random2}${random3}${random4}`
+  };
+  context.setState(state);
+  await context.sendText('遊戲開始！輸入四個數字來猜！');
+}
+
+async function GuessNumberAnswer(context) {
+  await context.sendText(context.state.number.answer);
+}
+
+async function Guess(context) {
+  if (!context.state.number.start) {
+    await context.sendText('輸入「猜數字」開始！');
+    return;
+  }
+  const answer = context.state.number.answer;
+
+  const num = context.event.text;
+  // if (num.length != 4) {
+  //   await context.sendText('必須是四個數字！');
+  //   return;
+  // }
+  for (let i = 0; i < 3; i++) {
+    for (let j = i + 1; j < 4; j++) {
+      if (num[i] == num[j]) {
+        await context.sendText('必須是四個「相異」數字！');
+        return;
+      }
+    }
+  }
+  let a = 0;
+  let b = 0;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (num[i] == answer[j]) {
+        if (i == j) a++;
+        else b++;
+      }
+    }
+  }
+
+  if (a == 4) {
+    const user = await context.getUserProfile();
+    const name = user.displayName;
+
+    let state = context.state;
+    state.number = {
+      start: false,
+    };
+    context.setState(state);
+    await context.sendText(`Bingo！${name} 猜對了！`);
+  } else {
+    await context.sendText(`${a} A ${b} B`);
+  }
+}
+
+
 async function Roller(context) {
+  const isInteger = value => parseInt(value) == value;
   const p = Number(context.event.text.split(" ")[1]);
   const people = isInteger(p) ? p : 2;
 
@@ -89,5 +165,7 @@ module.exports = async function App(context) {
     text(/^比大小/, Roller),
     text('骰', Roll),
     text('猜數字', GuessNumber),
+    text('答案', GuessNumberAnswer),
+    text(/^\d\d\d\d$/, Guess),
   ]);
 };
