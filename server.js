57bd7e93bddfb7b9d0667e65f9ee5543b7636246
tigercosmos <phy.tiger@gmail.com>
2019-12-23 17:00:04 +0800

    warm cache

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const { initializeServer } = require('bottender');
+const {
+  initializeServer
+} = require('bottender');
 const minimist = require('minimist');
+const getInstagramUrl = require("./lib/getInstagramUrl")
 
 const argv = minimist(process.argv.slice(2));
 
 const isConsole = argv.console;
 
 if (isConsole) {
-  initializeServer({ isConsole });
+  initializeServer({
+    isConsole
+  });
 } else {
   const server = initializeServer();
+
+  // warm cache first
+  (async () => {
+    await getInstagramUrl("girl");
+    await getInstagramUrl("food");
+    await getInstagramUrl("constellation");
+  })();
+
 
   const port = process.env.PORT || 8080;
   server.listen(port, () => {
