19c2796977efa7a239d1e4965e8bc93a5e7f7358
tigercosmos <phy.tiger@gmail.com>
2019-12-31 00:08:21 +0800

    start server later

--- server.js
+++ server.js
@@ -20,11 +20,10 @@ if (isConsole) {
     await getInstagramUrl("girl");
     await getInstagramUrl("food");
     await getInstagramUrl("constellation");
+
+    const port = process.env.PORT || 8080;
+    server.listen(port, () => {
+      console.log(`server is listening on ${port} port...`);
+    });
   })();
-
-
-  const port = process.env.PORT || 8080;
-  server.listen(port, () => {
-    console.log(`server is listening on ${port} port...`);
-  });
 }
